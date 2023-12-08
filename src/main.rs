b1600670211969666821a048d055e5389cd29776
Andreas Brändström <andreas.brandstrom@omegapoint.se>
2023-12-08 13:02:47 +0100

    solved day 4

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ fn main() {
     if false {
         day_1::calibration_value();
         day_2::impossible_games();
+        day_3::schematic_sum();
     }
-    day_3::schematic_sum();
+    
+    day_4::scratchcards_ponts();
 }
 
+mod day_4;
 mod day_3;
 mod day_2;
 mod day_1;
