f008bde748cb21318fa54b32dea57a19747bff8e
Andreas Brändström <andreas.brandstrom@omegapoint.se>
2023-12-11 13:22:48 +0100

    solved day 6

---
+++ src/day_6.rs
@@ -0,0 +1,34 @@
+pub(crate) fn boat_race() {
+    let data = 
+        "Time:        35696887
+        Distance:   213116810861248";
+
+    let (time, distance) = data.split_once('\n').unwrap();
+
+    let time: Vec<u64>  = time.split_whitespace().filter_map(|s| s.parse().ok()).collect();
+    let distance: Vec<u64> = distance.split_whitespace().filter_map(|s| s.parse().ok()).collect();
+
+    let races:Vec<(&u64,&u64)> = time.iter().zip(distance.iter()).collect();
+
+    println!("{races:?}");
+  
+    let sum: u64 = races.iter()
+        .map(|&race| number_of_ways_to_win(race))
+        .fold(1, |acc, n| acc*n);
+
+    println!("Sum: {} ",sum)
+}
+
+fn number_of_ways_to_win(race: (&u64,&u64)) -> u64 {
+    let total_time = *race.0 as f64;
+    let distance_record = *race.1 as f64;
+    let d_1 = 0.5*(total_time + f64::sqrt(total_time.powi(2) - 4.0*(distance_record + 1.0)));
+    let d_2 = 0.5*(total_time - f64::sqrt(total_time.powi(2) - 4.0*(distance_record + 1.0)));
+
+    let start = d_2.ceil() as u32;
+    let finish = d_1.floor() as u32;
+
+    let number_of_ways = Vec::from_iter(start..=finish).len() as u64;
+
+    number_of_ways   
+}
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,13 @@ fn main() {
         day_2::impossible_games();
         day_3::schematic();
         day_4::scratchcards();
+        day_5::seed_to_destination();
     }
 
-    day_5::seed_to_destination();
-    
-    
+    day_6::boat_race();
 }
 
-
+mod day_6;
 mod day_5;
 mod day_4;
 mod day_3;
